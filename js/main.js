a34277d5131fec542ae30f18779f7ecd9daf9d13
sagicary <sagicary@gmail.com>
2017-05-12 12:03:42 -0300

    Validando datos

--- js/main.js
+++ js/main.js
@@ -2,11 +2,15 @@ function mostrarData(){
 	var nombre=document.getElementById("nombre").value;
 	var apellido=document.getElementById("apellido").value;
 	var email=document.getElementById("mail").value;
+	var dire=document.getElementById("direccion").value;
+	var fono=document.getElementById("fono").value;
 	var mostrar = document.getElementById("clientes");
 	if (nombre=="" || apellido=="" || mail==""){
 		alert("No puede dejar campos vacios");
-	}
-	mostrar.innerHTML = ("<center><p><h1>Datos usuario: </h1><p><b>Nombre:</b>"+nombre+
+	}else{
+		mostrar.innerHTML = ("<center><p><h1>Datos usuario: </h1><p><b>Nombre:</b>"+nombre+
 		"</p><b>Apellido: </b>"+apellido+
 		"</p><b>Email: </b>"+email+"</center>");
+	}
+	
 }
