be7d6910391ff8bf582e2e8a97d3a694dbb1a7b6
sagicary <sagicary@gmail.com>
2017-05-12 11:52:40 -0300

    agregando validacion

--- js/main.js
+++ js/main.js
@@ -3,6 +3,9 @@ function mostrarData(){
 	var apellido=document.getElementById("apellido").value;
 	var email=document.getElementById("mail").value;
 	var mostrar = document.getElementById("clientes");
+	if (nombre=="" || apellido=="" || mail==""){
+		alert("No puede dejar campos vacios");
+	}
 	mostrar.innerHTML = ("<center><p><h1>Datos usuario: </h1><p><b>Nombre:</b>"+nombre+
 		"</p><b>Apellido: </b>"+apellido+
 		"</p><b>Email: </b>"+email+"</center>");
