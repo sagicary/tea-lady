461079ed8efddb4f6155904274fab2f03102a857
Sagicary <sagicary@gmail.com>
2017-05-13 19:05:48 -0300

    Ya muestra los datos y los valida

--- js/main.js
+++ js/main.js
@@ -4,13 +4,19 @@ function mostrarData(){
 	var email=document.getElementById("mail").value;
 	var dire=document.getElementById("direccion").value;
 	var fono=document.getElementById("fono").value;
+	var te=document.getElementById("seleccion").value;
+	var check=document.getElementById("check").checked;
+	console.log(check);
 	var mostrar = document.getElementById("clientes");
-	if (nombre=="" || apellido=="" || mail==""){
+	if (nombre=="" || apellido=="" || email=="" || dire=="" || fono=="" || te=="false" || check==""){
 		alert("No puede dejar campos vacios");
 	}else{
-		mostrar.innerHTML = ("<center><p><h1>Datos usuario: </h1><p><b>Nombre:</b>"+nombre+
-		"</p><b>Apellido: </b>"+apellido+
-		"</p><b>Email: </b>"+email+"</center>");
+		mostrar.innerHTML = ("<center><p><h1>Datos usuario: </h1><p><b>Nombre: </b>"+nombre+
+		"<p><b>Apellido: </b>"+apellido+
+		"<p><b>Email: </b>"+email+
+		"<p><b>Dirección: </b>"+dire+
+		"<p><b>Teléfono: </b>"+fono+
+		"<p><b>Tipo de té: </b>"+te+"</p><b>Estás suscrito al newsletter :)</b></center>");
 	}
 	
 }
