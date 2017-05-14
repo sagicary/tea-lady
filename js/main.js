019bdeac01f8b97ed07ea3153e7e6a416401a452
Sagicary <sagicary@gmail.com>
2017-05-13 21:35:12 -0300

    Listo

--- js/main.js
+++ js/main.js
@@ -11,12 +11,12 @@ function mostrarData(){
 	if (nombre=="" || apellido=="" || email=="" || dire=="" || fono=="" || te=="" || check==""){
 		alert("No puede dejar campos vacios");
 	}else{
-		mostrar.innerHTML = ("<center><p><h1>Datos usuario: </h1><p><b>Nombre: </b>"+nombre+
+		mostrar.innerHTML = ("<div><center><p><h1>Datos usuario: </h1><p><b>Nombre: </b>"+nombre+
 		"<p><b>Apellido: </b>"+apellido+
 		"<p><b>Email: </b>"+email+
 		"<p><b>Dirección: </b>"+dire+
 		"<p><b>Teléfono: </b>"+fono+
-		"<p><b>Tipo de té: </b>"+te+"</p><b>Estás suscrito al newsletter :)</b></center>");
+		"<p><b>Tipo de té: </b>"+te+"</p><b>Estás suscrito al newsletter :)</b></center><div>");
 	}
 	
 }
